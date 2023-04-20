753e5ae0c9861e6460f6a50ef4466c43b553dfe2
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-04-20 20:13:15 +0200

    Create navbar and style it

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,5 +1,40 @@
+import { Link } from "react-router-dom";
 import "./navbar.scss";
 
+// icons
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
 export default function Navbar() {
-  return <h1>navbar</h1>;
+  return (
+    <div className="navbar">
+      <div className="left">
+        <Link to="/">
+          <span>Coolname</span>
+        </Link>
+        <HomeOutlinedIcon />
+        <DarkModeOutlinedIcon />
+        <GridViewOutlinedIcon />
+        <div className="search">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search" />
+        </div>
+      </div>
+      <div className="right">
+        <Person2OutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
+        <div className="user">
+          <img src="" alt="" />
+          <span>John Doe</span>
+        </div>
+      </div>
+    </div>
+  );
 }
