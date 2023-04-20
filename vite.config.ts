b644afec34eb1bc9bd3d9863f85c229321d46e74
Shay213 <123243481+Shay213@users.noreply.github.com>
2023-04-20 12:21:46 +0200

    Initial structure

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/social-media-app/',
+  plugins: [react()],
+  server: {
+    open:true
+  }
+})
